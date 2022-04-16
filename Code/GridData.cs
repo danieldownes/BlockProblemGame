d4a5226ba2e82d4e6537a193f708a0b859f494f4
Daniel Downes <dd@danieldownes.co.uk>
2022-04-17 00:34:02 +0100

    data.dat > GridData.cs

---
+++ Code/GridData.cs
@@ -0,0 +1,23 @@
+public class GridData
+{
+	/*
+	Enum enuColour
+    R = 0
+    G = 1
+    B = 2
+    Y = 3
+	End Enum
+	*/
+
+	public static int[,] Data = 
+	{
+		//  T  B  L  R  HasDiamond
+		{3, 1, 2, 0, 3, 1},
+		{3, 1, 3, 0, 3, 1},
+		{1, 2, 1, 0, 3, 1},
+		{1, 3, 2, 0, 1, 1},
+		{1, 3, 1, 0, 2, 0},
+		{2, 2, 1, 0, 3, 0},
+		{5, 3, 2, 0, 1, 0}
+	};	
+}
